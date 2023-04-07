4034151dff1b33a04d5ccf93eddefb7fd67fe1ee
Madhusudan Pandey <sales@3sitservices.com>
2023-04-07 16:16:25 +0530

    Initial commit

---
+++ app/src/main/java/in/it/drawerlayout/MainActivity.java
@@ -0,0 +1,56 @@
+package in.it.drawerlayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerlayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = findViewById(R.id.navigationview);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        Toast.makeText(getApplicationContext(), "Home Clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.close();
+                    case R.id.search:
+                        Toast.makeText(getApplicationContext(), "Search Clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.close();
+                    case R.id.cart:
+                        Toast.makeText(getApplicationContext(), "Cart Clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.close();
+                    case R.id.account:
+                        Toast.makeText(getApplicationContext(), "Account Clicked", Toast.LENGTH_SHORT).show();
+                        drawerLayout.close();
+                }
+                return false;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
